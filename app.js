10c83de7242919aaa2a2e2b5377d727a7bbc8492
naglec13 <naglec13@gmail.com>
2020-08-07 12:07:27 +0300

    added variables via const and let, used web functions alert() and prompt()

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert('Hello, man,  from JavaScript!!');
+
+let name = 'James'
+const lastName = 'Bond'
+var status = 'agent'
+let id = '00' + 7
+
+alert('Hello, ' + status + ' ' + id + ' ' +
+    name + ' ' + lastName
+    )
+alert('Now you name is ' + (name = 'Call'))
+const phrase = prompt('Enter your secret phrase: ')
+console.log('Your nickname is ' + name + '\n' + 'Your secret phrase is : ' + phrase)
