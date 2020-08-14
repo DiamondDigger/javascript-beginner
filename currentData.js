d64b325d7df7287fcf896006d3fa78ac458c655f
naglec13 <naglec13@gmail.com>
2020-08-14 18:16:25 +0300

    formatted the date to the usual form

--- currentData.js
+++ currentData.js
@@ -6,9 +6,20 @@ let currentDate = new Date()
 //     + currentDate.getFullYear())
 
 const stringDate = () => {
-    return currentDate.getDay() + '.'
-        + currentDate.getMonth() + '.'
-        + currentDate.getFullYear()
+    return format('day') + '.'
+        + format('month') + '.'
+        + format()
+}
+
+let format = (objectOfDate) => {
+    switch (objectOfDate){
+        case 'day':
+            return currentDate.getDay() < 10 ? '0' + currentDate.getDay() : currentDate.getDay()
+        case 'month':
+            return currentDate.getMonth() < 10 ? '0' + currentDate.getMonth() : currentDate.getMonth()
+        default :
+            return currentDate.getFullYear()
+    }
 }
 
 document.getElementById('currentDate').innerHTML = stringDate()
