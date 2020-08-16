2b105b671b53eabf7f1ea880b84ef79d8f847276
naglec13 <naglec13@gmail.com>
2020-08-16 23:50:08 +0300

    added dynamic and computed properties in object, decomposition

--- object.js
+++ object.js
@@ -1,9 +1,11 @@
 const policeman = {
     name: 'Jack',
     id: 25,
-    gun: true,
+    gun: 'glock',
     car: true,
     states: ['Mx', 'Br', 'St'],
+    'computed prop': 'Computed Value',
+    ['ID_token_' + (5 / 10 + 431).toFixed(0)] : 'egct#245',
     greet() {
         console.log('Im officer Jack, can I help you?')
     }
@@ -11,3 +13,13 @@ const policeman = {
 
 console.log(policeman)
 policeman.greet()
+console.log(policeman["computed prop"])
+console.log('gun was deleted : ', delete policeman.gun)
+console.log(policeman);
+policeman.states.push('Vu')
+console.log(policeman.states);
+
+policeman.id = undefined
+console.log('set policeman.id = undefined');
+const {name, id: idNumber = 313, states} = policeman
+console.log(name, idNumber, states);
