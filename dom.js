3dbd2164d5d6f6c9d4156543350067fe66aaeb54
naglec13 <naglec13@gmail.com>
2020-08-19 19:58:16 +0300

    added event - changed dom style on event

--- dom.js
+++ dom.js
@@ -12,18 +12,18 @@ console.log('domElement.innerText: ', domElement.innerText)
 console.log('domElement: ', domElement)
 console.log('headingH2: ', headingH2)
 console.log('nextHeadingH2: ', anotherHeadingH2)
-console.log('querySelectorAll("h2"): ', h2List )
+console.log('querySelectorAll("h2"): ', h2List)
 
 setTimeout(() => {
-    addStylesTo(h2List[h2List.length-1])
+    addStylesTo(h2List[h2List.length - 1])
 }, 2500)
 
 setTimeout(() => {
-    addStylesTo(anotherHeadingH2,'Hmm..It`s looks like..', 'red', '3rem')
+    addStylesTo(anotherHeadingH2, 'Hmm..It`s looks like..', 'red', '3rem')
 }, 5000)
 
 setTimeout(() => {
-    addStylesTo(headingH2, 'we`ve done it!!!','yellow', '2rem')
+    addStylesTo(headingH2, 'we`ve done it!!!', 'yellow', '2rem')
 }, 7000)
 
 function addStylesTo(node, text = 'this is best place for your ad!!', color = 'yellow', fontSize) {
@@ -34,3 +34,23 @@ function addStylesTo(node, text = 'this is best place for your ad!!', color = 'y
     node.style.padding = '1rem'
     node.style.fontSize = fontSize
 }
+
+headingH2.onclick = () => {
+    if (headingH2.style.color === 'yellow') {
+        headingH2.style.color = 'black';
+        headingH2.style.backgroundColor = 'white';
+    } else {
+        headingH2.style.color = 'yellow'
+        headingH2.style.backgroundColor = 'black'
+    }
+}
+
+anotherHeadingH2.addEventListener('dblclick', () => {
+    if (anotherHeadingH2.style.color === 'red') {
+        anotherHeadingH2.style.color = 'black';
+        anotherHeadingH2.style.backgroundColor = 'white';
+    } else {
+        anotherHeadingH2.style.color = 'red'
+        anotherHeadingH2.style.backgroundColor = 'black'
+    }
+})
