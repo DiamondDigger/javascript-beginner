974fc79027313bb87e9ac48af6d085fb080f36e7
naglec13 <naglec13@gmail.com>
2020-08-19 18:00:43 +0300

    added function with changing style of dom elem

--- dom.js
+++ dom.js
@@ -3,5 +3,18 @@ const domElement = document.getElementById('dom')
 console.dir(domElement)
 console.log(domElement)
 
-domElement.textContent = 'Welcome here. Lets try our Google Search Engine!'
-domElement.style.color = 'blue'
+console.log(domElement.innerHTML)
+console.log(domElement.innerText)
+
+setTimeout(() => {
+    addStylesTo(domElement)
+}, 2500)
+
+function addStylesTo(node) {
+    node.textContent = 'Welcome here. Lets try our Google Search Engine!'
+    node.style.color = 'blue'
+    node.style.textAlign = 'center'
+    node.style.backgroundColor = 'yellow'
+    node.style.padding = '2rem'
+
+}
