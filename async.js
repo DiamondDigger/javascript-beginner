f9cd1e60aa44ee1c9031bfe64bd676a4b7031126
naglec13 <naglec13@gmail.com>
2020-08-19 10:24:03 +0300

    wrapped setTimeout in another function

--- async.js
+++ async.js
@@ -1,24 +1,32 @@
-const timeout = setTimeout(() => {
-    console.log(`After ${sec}`)
-    console.log('end of setTimeout')
-}, sec = 6000)
+// const timeout = setTimeout(() => {
+//     console.log(`After ${sec}`)
+//     console.log('end of setTimeout')
+// }, sec = 6000)
+//
+// // clearTimeout(timeout)
+//
+// let index = 0
+// const interval = setInterval(() => {
+//     index++
+//     console.log(`After ${intervalTime}`)
+//     if (index === 5) {
+//         clearInterval(interval)
+//         console.log('1end of setInterval')
+//         console.log('2end of setInterval')
+//         console.log('3end of setInterval')
+//         console.log('4end of setInterval')
+//     }
+//
+// }, intervalTime = 1000)
+//
+// if (index === 5) {
+//     clearInterval(interval)
+// }
 
-// clearTimeout(timeout)
-
-let index = 0
-const interval = setInterval(() => {
-    index++
-    console.log(`After ${intervalTime}`)
-    if (index === 5) {
-        clearInterval(interval)
-        console.log('1end of setInterval')
-        console.log('2end of setInterval')
-        console.log('3end of setInterval')
-        console.log('4end of setInterval')
-    }
-
-}, intervalTime = 1000)
-
-if (index === 5) {
-    clearInterval(interval)
+const delay = (callback, wait = 1000) => {
+    setTimeout(callback, wait)
 }
+
+delay(() => {
+    console.log('After 2 seconds')
+}, 2000)
