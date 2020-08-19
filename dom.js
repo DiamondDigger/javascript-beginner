0137a357cffb79da5ece36a6eae69dbd8a1cd3ff
naglec13 <naglec13@gmail.com>
2020-08-19 16:40:49 +0300

    added work with dom, changed textContent and style.color attribute

---
+++ dom.js
@@ -0,0 +1,7 @@
+const domElement = document.getElementById('dom')
+
+console.dir(domElement)
+console.log(domElement)
+
+domElement.textContent = 'Welcome here. Lets try our Google Search Engine!'
+domElement.style.color = 'blue'
