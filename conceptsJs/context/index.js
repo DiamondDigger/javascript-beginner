39d04205c053efbd425fea1dbb4be0dec107e3bc
naglec13 <naglec13@gmail.com>
2020-08-20 18:09:35 +0300

    added .bind with arguments, .call and .apply

--- conceptsJs/context/index.js
+++ conceptsJs/context/index.js
@@ -7,10 +7,12 @@ const alien = {
     age: 245,
     sayHello: hello,
     sayHelloWindow: hello.bind(document),
-    logInfo() {
+    logInfo(job, phone) {
         console.group(`${this.name} info: `)
         console.log(`Alien name is ${this.name}`)
         console.log(`Alien age is ${this.age}`)
+        console.log(`Alien job is ${job}`)
+        console.log(`Alien phone is ${phone}`)
         console.groupEnd()
     }
 }
@@ -20,5 +22,7 @@ const human = {
     age: 45
 }
 
-alien.logInfo.bind(human)()
+alien.logInfo.bind(human,'mechanic', '23-34-2354')()
+alien.logInfo.call(human,'mechanic', '23-34-2354')
+alien.logInfo.apply(human,['mechanic', '23-34-2354'])
 
