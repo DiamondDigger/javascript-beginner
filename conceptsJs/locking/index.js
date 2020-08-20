0ddee9d09d84223566d82a2b0a3bad89a20331f7
naglec13 <naglec13@gmail.com>
2020-08-21 00:23:23 +0300

    more complex function with locking

--- conceptsJs/locking/index.js
+++ conceptsJs/locking/index.js
@@ -37,3 +37,19 @@ ruUrl('yandex')
 ruUrl('yahoo')
 
 ruUrl('amazon')
+
+function logPerson(){
+    console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+}
+
+const person1 = {name: 'Mike', age: 23, job: 'driver'}
+const person2 = {name: 'Michal', age: 27, job: 'carpenter'}
+
+function bind(context, fn){
+    return function(...args){
+        fn.apply(context, args)
+    }
+}
+
+bind(person1, logPerson)()
+bind(person2, logPerson)()
