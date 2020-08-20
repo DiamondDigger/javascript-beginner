6089f2db00421da489519fd1400799e072f6e5f0
naglec13 <naglec13@gmail.com>
2020-08-20 23:47:12 +0300

    added locking function with two arguments - one in each function

--- conceptsJs/locking/index.js
+++ conceptsJs/locking/index.js
@@ -1,8 +1,22 @@
 function createCalcFunction(n) {
-    return function() {
+    return function () {
         console.log(1000 * n)
     }
 }
 
 const calcFunction = createCalcFunction(34)
 calcFunction()
+
+function createIncrementor(n) {
+    return function (num) {
+        return n + num
+    }
+}
+
+const addOne = createIncrementor(1)
+const addTen = createIncrementor(10)
+
+console.log('1+19=', addOne(19))
+console.log('1+0=',addOne(0))
+console.log('10+34=',addTen(34))
+console.log('10+(-2)=',addTen(-2))
