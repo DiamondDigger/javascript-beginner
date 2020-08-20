4ffdd5f00bc75f711381eab72706a1d20731e578
naglec13 <naglec13@gmail.com>
2020-08-20 12:11:40 +0300

    added function in Object.prototype

---
+++ conceptsJs/prototype/index.js
@@ -0,0 +1,12 @@
+const alien = new Object({
+    name: 'Rudolf',
+    age: 250,
+    greet(){
+        console.log('Hello from Pluto!')
+    }
+})
+
+Object.prototype.sayHello = function(){
+    console.log('Hello')
+}
+
