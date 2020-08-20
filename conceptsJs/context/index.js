0935c6e246157d5ffa5d8e6f253d99a328e3f554
naglec13 <naglec13@gmail.com>
2020-08-20 23:39:09 +0300

    added locking function

--- conceptsJs/context/index.js
+++ conceptsJs/context/index.js
@@ -22,7 +22,26 @@ const human = {
     age: 45
 }
 
-alien.logInfo.bind(human,'mechanic', '23-34-2354')()
-alien.logInfo.call(human,'mechanic', '23-34-2354')
-alien.logInfo.apply(human,['mechanic', '23-34-2354'])
+alien.logInfo.bind(human, 'mechanic', '23-34-2354')()
+alien.logInfo.call(human, 'mechanic', '23-34-2354')
+alien.logInfo.apply(human, ['mechanic', '23-34-2354'])
 
+const array = [1, 2, 3, 4, 5]
+console.log(array);
+//old way
+// const multBy = (array, n) => {
+//     return array.map((i) => {
+//         return i * n
+//     })
+// }
+
+Array.prototype.multBy = function (n) {
+    return this.map((i) => {
+        return i * n
+    })
+}
+console.log(array.multBy(10))
+
+const anotherArray = array.multBy(5)
+
+console.log(anotherArray.multBy(20));
