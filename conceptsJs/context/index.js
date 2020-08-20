f7a735cd9e2aa373b1d7247fdaa3dfc3eb6549c6
naglec13 <naglec13@gmail.com>
2020-08-20 17:51:10 +0300

    added console.group()/console.groupEnd()

--- conceptsJs/context/index.js
+++ conceptsJs/context/index.js
@@ -8,8 +8,10 @@ const alien = {
     sayHello: hello,
     sayHelloWindow: hello.bind(document),
     logInfo() {
+        console.group(`${this.name} info: `)
         console.log(`Alien name is ${this.name}`)
         console.log(`Alien age is ${this.age}`)
+        console.groupEnd()
     }
 }
 
