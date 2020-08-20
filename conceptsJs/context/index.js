3a6e752d0bced06b37fa33f66b7ad52ddf228c0f
naglec13 <naglec13@gmail.com>
2020-08-20 13:03:16 +0300

    changed context in referenced function

--- conceptsJs/context/index.js
+++ conceptsJs/context/index.js
@@ -5,7 +5,18 @@ function hello() {
 const alien = {
     name: 'Djiko',
     age: 245,
-    sayHello: hello
-
+    sayHello: hello,
+    sayHelloWindow: hello.bind(document),
+    logInfo() {
+        console.log(`Alien name is ${this.name}`)
+        console.log(`Alien age is ${this.age}`)
+    }
 }
+
+const human = {
+    name: 'Baculuy',
+    age: 45
+}
+
+alien.logInfo.bind(human)()
 
