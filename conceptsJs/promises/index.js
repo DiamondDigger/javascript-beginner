b32b7ab2ff5f89ae6141d34cff5becd026c43408
naglec13 <naglec13@gmail.com>
2020-08-21 15:54:56 +0300

    added .catch and .finally

--- conceptsJs/promises/index.js
+++ conceptsJs/promises/index.js
@@ -1,24 +1,24 @@
 console.log('Requesting data...')
 
-//setTimouts
-// setTimeout(()=>{
-//     console.log('Preparing data...')
-//
-//     const backendData = {
-//         server: 'aws',
-//         port: 2000,
-//         status: 'working'
-//     }
-//
-//     setTimeout(()=>{
-//         backendData.modified = true
-//         console.log('Data received', backendData)
-//     } ,2000)
-//
-// }, 5000)
+setTimouts
+setTimeout(()=>{
+    console.log('Preparing data...')
+
+    const backendData = {
+        server: 'aws',
+        port: 2000,
+        status: 'working'
+    }
+
+    setTimeout(()=>{
+        backendData.modified = true
+        console.log('Data received', backendData)
+    } ,2000)
+
+}, 5000)
 
 const p = new Promise((resolve, reject) => {
-    setTimeout(()=>{
+    setTimeout(() => {
         console.log('Preparing data...')
         const backendData = {
             name: 'aws',
@@ -26,7 +26,7 @@ const p = new Promise((resolve, reject) => {
             status: 'working'
         }
         resolve(backendData)
-    },5000)
+    }, 5000)
 })
 
 p.then((data) => {
@@ -34,8 +34,20 @@ p.then((data) => {
         setTimeout(() => {
             data.modified = true
             resolve(data)
-        },2000)
+        }, 2000)
     })
-}).then((clientData)=>{
-    console.log('Data received', clientData)
 })
+    .then((clientData) => {
+        console.log('Data received', clientData)
+    })
+    .catch(err => console.error('Error: ', err))
+    .finally(() => console.log('Text from finally block'))
+
+const sleep = ms => {
+    return new Promise(resolve => {
+        resolve(ms)
+    },ms)
+}
+
+sleep(2000).then(ms => console.log(`after ${ms/1000} sec`))
+sleep(3000).then(ms => console.log(`after ${ms/1000} sec`))
