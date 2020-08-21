56af0ad3c818ec1bf90393d28812515de14ddd37
naglec13 <naglec13@gmail.com>
2020-08-21 20:36:48 +0300

    added get and set methods, descriptor configurable

--- conceptsJs/object/index.js
+++ conceptsJs/object/index.js
@@ -2,10 +2,20 @@ const person = Object.create({},{
     name: {
         value: 'Oleg',
         enumerable: true,
-        writable: true
+        writable: true,
+        configurable: true
     },
     age: {
-        value: 34
+        value: 57
+    },
+    birthYear: {
+        get(){
+            return new Date().getFullYear() - this.age
+        },
+        set(value){
+            console.log('Birthday set in', value)
+            document.body.style.backgroundImage = 'url(https://c.wallhere.com/photos/85/a9/1600x900_px_minimalism_NASA_Pluto_space-682126.jpg!d)'
+        }
     }
 })
 
