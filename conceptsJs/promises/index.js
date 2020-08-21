409963bf72749ea8bdd7d5b0336b0cdb37e3a5a3
naglec13 <naglec13@gmail.com>
2020-08-21 12:02:40 +0300

    refactor existing code with promises

--- conceptsJs/promises/index.js
+++ conceptsJs/promises/index.js
@@ -1,17 +1,42 @@
 console.log('Requesting data...')
 
-setTimeout(()=>{
-    console.log('Preparing data...')
+//setTimouts
+// setTimeout(()=>{
+//     console.log('Preparing data...')
+//
+//     const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//     }
+//
+//     setTimeout(()=>{
+//         backendData.modified = true
+//         console.log('Data received', backendData)
+//     } ,2000)
+//
+// }, 5000)
 
-    const backendData = {
-        server: 'aws',
-        port: 2000,
-        status: 'working'
-    }
-
+const p = new Promise((resolve, reject) => {
     setTimeout(()=>{
-        backendData.modified = true
-        console.log('Data received', backendData)
-    } ,2000)
+        console.log('Preparing data...')
+        const backendData = {
+            name: 'aws',
+            port: 2000,
+            status: 'working'
+        }
+        resolve(backendData)
+    },5000)
+})
 
-}, 5000)
+p.then((data) => {
+    const p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.modified = true
+            resolve(data)
+        },2000)
+    })
+    p2.then((clientData)=>{
+        console.log('Data received', clientData)
+    })
+})
