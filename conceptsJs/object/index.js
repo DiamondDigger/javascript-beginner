e06faec1d5a0bc27afd1c6a5f4cd26f5dcd6c248
naglec13 <naglec13@gmail.com>
2020-08-21 20:46:25 +0300

    added method in prototype argument in Object.create(), added .hasOwnProperty in for in loop

--- conceptsJs/object/index.js
+++ conceptsJs/object/index.js
@@ -1,27 +1,36 @@
-const person = Object.create({},{
-    name: {
-        value: 'Oleg',
-        enumerable: true,
-        writable: true,
-        configurable: true
-    },
-    age: {
-        value: 57
-    },
-    birthYear: {
-        get(){
-            return new Date().getFullYear() - this.age
-        },
-        set(value){
-            console.log('Birthday set in', value)
-            document.body.style.backgroundImage = 'url(https://c.wallhere.com/photos/85/a9/1600x900_px_minimalism_NASA_Pluto_space-682126.jpg!d)'
+const person = Object.create(
+    {
+        calculateBirthYear() {
+            console.log('BirthYear is ', new Date().getFullYear() - this.age)
         }
-    }
-})
+    },
+    {
+        name: {
+            value: 'Oleg',
+            enumerable: true,
+            writable: true,
+            configurable: true
+        },
+        age: {
+            value: 57
+        },
+        birthYear: {
+            get() {
+                return new Date().getFullYear() - this.age
+            },
+            set(value) {
+                console.log('Birthday set in', value)
+                document.body.style.backgroundImage =
+                    'url(https://c.wallhere.com/photos/85/a9/1600x900_px_minimalism_NASA_Pluto_space-682126.jpg!d)'
+            }
+        }
+    })
 
 console.log(person)
 
 person.name = 'Snake'
-for (let key in person){
-    console.log('Key', key, person[key])
+for (let key in person) {
+    if (person.hasOwnProperty(key)) {
+        console.log('Key', key, person[key]);
+    }
 }
