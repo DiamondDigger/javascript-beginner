52558fcddcfa6786fa70e9904d34f0e0f06bc21f
naglec13 <naglec13@gmail.com>
2020-08-23 17:11:08 +0300

    added function proxy with apply method

--- conceptsJs/proxy/index.js
+++ conceptsJs/proxy/index.js
@@ -6,12 +6,12 @@ const alien = {
     enemy: false
 }
 
-const op = new Proxy(alien,{
-    get(target, prop){
+const op = new Proxy(alien, {
+    get(target, prop) {
         console.log(`Getting prop '${prop}' from target...`)
         return target[prop]
     },
-    set(target, prop,value){
+    set(target, prop, value) {
         if (prop in target) {
             target[prop] = value;
             console.log(`Successfully changed prop '${prop}' in target `)
@@ -19,14 +19,24 @@ const op = new Proxy(alien,{
             throw new Error(`No such field '${prop}' in target object!`)
         }
     },
-    has(target, prop){
+    has(target, prop) {
         console.log(Object.keys(target))
         const keys = Object.keys(target);
         return keys.includes(prop + '')
     },
-    deleteProperty(target, prop){
+    deleteProperty(target, prop) {
         console.log(`Deleting '${prop}'...`)
         delete target[prop]
         return true
+    }
+})
+
+const log = (text = '') => `Log: ${text}`
+
+const fp = new Proxy(log,{
+    apply(target, thisArg, argArray) {
+        console.log('Applying log function...')
+
+        return target.apply(thisArg, argArray).toUpperCase()
     }
 })
