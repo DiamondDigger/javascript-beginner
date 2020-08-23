2bbb60a8e7e2a990287d99506b358a5744acb821
naglec13 <naglec13@gmail.com>
2020-08-23 12:21:33 +0300

    refactored promise/then with async/await

--- conceptsJs/asyncAwait/index.js
+++ conceptsJs/asyncAwait/index.js
@@ -20,3 +20,20 @@ function fetchTodos() {
 fetchTodos()
     .then(data => console.log('Data: ', data))
     .catch(e => console.error(e))
+
+async function fetchAsyncTodos(){
+    console.log('Async fetch todo started...')
+    try{
+        await delay(2000)
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log('Data: ', data)
+    }catch(e){
+        console.error('Error: ', e)
+    }finally{
+        console.log('yap!')
+    }
+}
+
+fetchAsyncTodos()
+    .then(()=> console.log('Everything is done! Congrats!!!'))
