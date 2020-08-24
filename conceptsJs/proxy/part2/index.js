487289fec1ba924cac780057bd73504e9a3bd9ae
naglec13 <naglec13@gmail.com>
2020-08-24 16:59:27 +0300

    added hidden (private) fields in object with proxy

--- conceptsJs/proxy/part2/index.js
+++ conceptsJs/proxy/part2/index.js
@@ -1,3 +1,4 @@
+//add default value
 const addDefaultValue = (target, defaultValue = 0 ) => {
 
     return new Proxy(target, {
@@ -13,3 +14,21 @@ const position = {
 }
 
 const p = addDefaultValue(position, 5)
+
+//hidden properties
+const withHiddenParams = (target, prefix = '_')=>{
+    return new Proxy(target , {
+        has:(obj, prop)=> prop in obj && !prop.startsWith(prefix),
+        ownKeys: (obj) => Reflect.ownKeys(obj)
+            .filter(p => p in obj && !p.startsWith(prefix)),
+        get: (obj, prop, receiver)=> prop in receiver ? obj[prop] : undefined
+    })
+}
+
+const data = withHiddenParams( {
+    planet: 'Pluto',
+    name: 'Miraco',
+    _ship: 'supersonic',
+    age: 567,
+    _enemy: false
+})
