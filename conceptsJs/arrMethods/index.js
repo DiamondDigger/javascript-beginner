a9991d4534e34ec354299b0962adfcca8fe79ec2
naglec13 <naglec13@gmail.com>
2020-08-25 12:22:12 +0300

    added .forEach, .map, .filter, .reduce

---
+++ conceptsJs/arrMethods/index.js
@@ -0,0 +1,20 @@
+const data = [
+    {name:'Jo', id: 11, role:'officer'},
+    {name:'Kenny', id: 22, role:'officer'},
+    {name:'Billy', id: 1, role:'prisoner', danger: false, term: 2},
+    {name:'ken', id: 2, role:'prisoner', danger: true, term: 8},
+    {name:'Jenny', id: 3, role:'prisoner', danger: false, term: 4}
+]
+
+data.forEach(person => console.log(person))
+
+const countOfPeople = data.map(person => person)
+console.log(countOfPeople)
+const protocol = data.filter(person => person.role ==='prisoner')
+console.log('protocol', protocol)
+
+const amountOfYearsInJail = data.filter(person => person.role === 'prisoner').reduce((term, person) => {
+    return term += person.term
+}, 0)
+
+console.log(amountOfYearsInJail)
