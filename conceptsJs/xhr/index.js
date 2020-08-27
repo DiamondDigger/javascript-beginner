43355204ba723e18797f1621e31bd2ff6974548b
naglec13 <naglec13@gmail.com>
2020-08-27 16:41:59 +0300

    added POST request via xhr

--- conceptsJs/xhr/index.js
+++ conceptsJs/xhr/index.js
@@ -2,12 +2,13 @@ const requestUrl = 'https://jsonplaceholder.typicode.com/users'
 
 const xhr = new XMLHttpRequest()
 
-function sendRequest(method, url){
+function sendRequest(method, url, body = null) {
     return new Promise((resolve, reject) => {
 
         xhr.open(method, url)
 
         xhr.responseType = 'json'
+        xhr.setRequestHeader('Content-Type', 'application/json')
 
         xhr.onload = () => {
             if (xhr.status >= 400) {
@@ -21,11 +22,19 @@ function sendRequest(method, url){
             reject(xhr.response)
         }
 
-        xhr.send()
+        xhr.send(JSON.stringify(body));
 
     })
 }
 
-sendRequest('GET', requestUrl)
-    .then((data)=> console.log(data))
-    .catch((e)=> console.error(e))
+// sendRequest('GET', requestUrl)
+//     .then((data) => console.log(data))
+//     .catch((e) => console.error(e))
+
+const body = {
+    name: 'Linkoln',
+    project: 'Siriuis',
+    id: 101
+}
+
+sendRequest('POST', requestUrl, body)
