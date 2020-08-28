6dbb70ba338033505e09ced8a7cd0b13712f2162
naglec13 <naglec13@gmail.com>
2020-08-28 13:22:43 +0300

    added spread on objects

--- conceptsJs/spreadRest/index.js
+++ conceptsJs/spreadRest/index.js
@@ -1,36 +1,56 @@
-const zooRussia = [
-    'Lion: 4',
-    'bird: 34',
-    'crocodile: 2',
-    'snake: 3'
-]
+// const zooRussia = [
+//     'Lion: 4',
+//     'bird: 34',
+//     'crocodile: 2',
+//     'snake: 3'
+// ]
+//
+// const zooEurope = [
+//     'Lion: 7',
+//     'bird: 25',
+//     'wolf: 10',
+//     'eagle: 15',
+//     'elephant: 2'
+// ]
+//
+// console.log('zoo in Russia:', zooRussia)
+// console.log('zoo in Europe:', zooEurope)
+//
+// console.log('zoo in Russia(...):', ...zooRussia)
+// console.log('zoo in Europe(...):', ...zooEurope)
+//
+//
+// const showAnimal = (array) => console.log(...array)
+// console.log('From function:')
+// showAnimal(zooEurope)
+//
+//
+// const allAnimals = [...zooRussia, ...zooEurope]
+// console.log('all animals: ', allAnimals)
+//
+// const numbers = [1,3,5,63,733,0]
+//
+// const arr = [...numbers]
+// console.log(arr)
 
-const zooEurope = [
-    'Lion: 7',
-    'bird: 25',
-    'wolf: 10',
-    'eagle: 15',
-    'elephant: 2'
-]
 
-console.log('zoo in Russia:', zooRussia)
-console.log('zoo in Europe:', zooEurope)
-
-console.log('zoo in Russia(...):', ...zooRussia)
-console.log('zoo in Europe(...):', ...zooEurope)
+const russianFauna = {
+    Lion: 4,
+    bird: 34,
+    crocodile: 2,
+    snake: 3
+}
 
 
-const showAnimal = (array) => console.log(...array)
-console.log('From function:')
-showAnimal(zooEurope)
+const europeFauna = {
+    Lion: 7,
+    bird: 25,
+    wolf: 10,
+    eagle: 15,
+    elephant: 2
+}
 
-
-const allAnimals = [...zooRussia, ...zooEurope]
-console.log('all animals: ', allAnimals)
-
-const numbers = [1,3,5,63,733,0]
-
-const arr = [...numbers]
-console.log(arr)
-
-
+console.log('Europe:',{...europeFauna})
+console.log('Russia:',{...russianFauna})
+console.log({...europeFauna,...russianFauna})
+console.log({...russianFauna,...europeFauna})
