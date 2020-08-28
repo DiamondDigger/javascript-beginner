5339ee18584b1ca1efc1f1050c18dba47f94683b
naglec13 <naglec13@gmail.com>
2020-08-28 21:26:49 +0300

    added rest

--- conceptsJs/spreadRest/index.js
+++ conceptsJs/spreadRest/index.js
@@ -1,56 +1,71 @@
-// const zooRussia = [
-//     'Lion: 4',
-//     'bird: 34',
-//     'crocodile: 2',
-//     'snake: 3'
-// ]
-//
-// const zooEurope = [
-//     'Lion: 7',
-//     'bird: 25',
-//     'wolf: 10',
-//     'eagle: 15',
-//     'elephant: 2'
-// ]
-//
-// console.log('zoo in Russia:', zooRussia)
-// console.log('zoo in Europe:', zooEurope)
-//
-// console.log('zoo in Russia(...):', ...zooRussia)
-// console.log('zoo in Europe(...):', ...zooEurope)
-//
-//
-// const showAnimal = (array) => console.log(...array)
-// console.log('From function:')
-// showAnimal(zooEurope)
-//
-//
-// const allAnimals = [...zooRussia, ...zooEurope]
-// console.log('all animals: ', allAnimals)
-//
+// // const zooRussia = [
+// //     'Lion: 4',
+// //     'bird: 34',
+// //     'crocodile: 2',
+// //     'snake: 3'
+// // ]
+// //
+// // const zooEurope = [
+// //     'Lion: 7',
+// //     'bird: 25',
+// //     'wolf: 10',
+// //     'eagle: 15',
+// //     'elephant: 2'
+// // ]
+// //
+// // console.log('zoo in Russia:', zooRussia)
+// // console.log('zoo in Europe:', zooEurope)
+// //
+// // console.log('zoo in Russia(...):', ...zooRussia)
+// // console.log('zoo in Europe(...):', ...zooEurope)
+// //
+// //
+// // const showAnimal = (array) => console.log(...array)
+// // console.log('From function:')
+// // showAnimal(zooEurope)
+// //
+// //
+// // const allAnimals = [...zooRussia, ...zooEurope]
+// // console.log('all animals: ', allAnimals)
+// //
 // const numbers = [1,3,5,63,733,0]
+// //
+// // const arr = [...numbers]
+// // console.log(arr)
 //
-// const arr = [...numbers]
-// console.log(arr)
+//
+// const russianFauna = {
+//     Lion: 4,
+//     bird: 34,
+//     crocodile: 2,
+//     snake: 3
+// }
+//
+//
+// const europeFauna = {
+//     Lion: 7,
+//     bird: 25,
+//     wolf: 10,
+//     eagle: 15,
+//     elephant: 2
+// }
+//
+// console.log('Europe:',{...europeFauna})
+// console.log('Russia:',{...russianFauna})
+// console.log({...europeFauna,...russianFauna})
+// console.log({...russianFauna,...europeFauna})
+//
+// console.log(Math.max(1,3,4,5,7,12))
+// console.log(Math.max(...numbers))
+//
 
+const numbers = [1,2,4,5,6,7,19]
 
-const russianFauna = {
-    Lion: 4,
-    bird: 34,
-    crocodile: 2,
-    snake: 3
+function sum (a,b, ...rest) {
+    console.log('rest: ',rest)
+    return a+b+rest.reduce((restSum,i)=> restSum += i
+    ,0)
 }
 
-
-const europeFauna = {
-    Lion: 7,
-    bird: 25,
-    wolf: 10,
-    eagle: 15,
-    elephant: 2
-}
-
-console.log('Europe:',{...europeFauna})
-console.log('Russia:',{...russianFauna})
-console.log({...europeFauna,...russianFauna})
-console.log({...russianFauna,...europeFauna})
+console.log(sum(2, 5));
+console.log(sum(...numbers))
