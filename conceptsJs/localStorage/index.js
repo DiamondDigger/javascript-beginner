583aad7123b250516e89cd80b13878c1a59cdea1
naglec13 <naglec13@gmail.com>
2020-08-29 23:52:28 +0300

    added local storage with getItem, setItem, removeItem, JSON.stringify and JSON.parse

---
+++ conceptsJs/localStorage/index.js
@@ -0,0 +1,19 @@
+const man = {
+    name: 'Jack',
+    age: 23
+}
+
+
+console.log(localStorage.getItem(man));
+console.log(man)
+localStorage.setItem('person', JSON.stringify(man))
+console.log('from local storage without parsing',localStorage.getItem('person'));
+
+const person = JSON.parse(localStorage.getItem('person'))
+console.log('from local storage',person)
+person.name = 'Vendor'
+console.log(person)
+
+// console.log('deleted from local storage',localStorage.removeItem('person'));
+
+console.log(localStorage.getItem('person'));
