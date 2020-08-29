d3f509311f741374a4ceeb2c763e4d9320385ce5
naglec13 <naglec13@gmail.com>
2020-08-29 22:44:04 +0300

    added destruction on an object

--- conceptsJs/destruction/index.js
+++ conceptsJs/destruction/index.js
@@ -15,3 +15,24 @@ console.log(sum, others)
 const [anotherSum, sub = 'Value is undefined!', ...anotherOthers] = mathArray
 console.log(anotherSum, sub, anotherOthers)
 
+const maserati = {
+    country: 'Italy',
+    year: 1987,
+    engine: {
+        type: 'V6',
+        volume: '6.0 L'
+    }
+}
+
+const {country, year, engine:
+    { type:
+        engineType = 'undefined',
+        volume: capacityOfOneCylinder = 'unknown'} } = maserati
+console.log('Maserati: ' ,country, year, engineType, capacityOfOneCylinder)
+
+function carInfo({engine: specs, year: birthday = 'unknown', country: home = 'cant find'}){
+    console.log(specs, birthday, home)
+}
+
+console.log('From function carInfo ____________________________ ')
+carInfo(maserati)
